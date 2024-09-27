7ca280795cd61ecdb474ca91c9b5509a6de029d9
VictorBaron1 <victorb104@nycstudents.net>
2024-09-27 09:08:09 -0400

    Add files via upload

---
+++ Scoreboard.java
@@ -0,0 +1,19 @@
+public class Scoreboard 
+{
+   
+    private String teamOne;
+    private String teamTwo;
+    private int getTeamOneScore;
+    private int getTeamTwoScore;
+    private String activeTeam;
+    public Scoreboard(String one , String two)
+    {
+            teamOne = one;
+            teamTwo = two;
+            activeTeam = teamOne;
+    }
+    public String getScore()
+    {
+        return null;
+    }
+}
